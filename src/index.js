0e4f500134c128d9521b6e338d8ade0602941a6d
iamindika <ikolamba2@gmail.com>
2022-03-06 13:15:26 -0500

    add state and nav to app component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { getTopStories } from './utils/api';
+import './index.css';
 
-function App() {
-  return (
-    <React.Fragment>
-      <h1>Hello Clover & Scruffy!</h1>
-      <p>I miss you Scruffy!!</p>
-    </React.Fragment>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      selected: 'Top'
+    }
+  }
+
+  render() {
+    const {selected} = this.state;
+    const storyTypes = ['Top', 'New'];
+
+    return (
+      <ul className="container nav row">
+        { storyTypes.map(type => (
+          <li 
+            key={type} 
+          >
+            <button
+              className="btn"
+              style={selected === type ? {color: 'red'} : null}
+              onClick={() => this.setState({ selected: type })}
+            >{type}</button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
 }
 
 ReactDOM.render(
