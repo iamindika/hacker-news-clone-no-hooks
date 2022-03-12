b59c7c5ad2ba30599ec36e263f6b07c5f98bc294
iamindika <ikolamba2@gmail.com>
2022-03-12 18:18:59 -0500

    add ui for error and user data

--- src/components/User.js
+++ src/components/User.js
@@ -2,33 +2,47 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Nav from './Nav';
 import Loading from './Loading';
+import {getUser} from '../utils/api';
 
 export default class User extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      user: null
+      user: null,
+      error: null
     }
   }
 
+  componentDidMount() {
+    getUser(this.props.username)
+      .then(user => this.setState({user}))
+      .catch(error => this.setState({error: error.message}));
+  }
+
   render() {
-    const {user} = this.state;
+    const {user, error} = this.state;
 
     return (
       <React.Fragment>
         <Nav/>
 
-        {!user && <Loading text="Fetching User"/>}
+        {!user && !error 
+          ? <Loading text="Fetching User"/>
+          : null}
+
+        {error && <p className="error">ERROR: {error}</p>}
+
+        {user && <pre>{JSON.stringify(user, null, 2)}</pre>}
       </React.Fragment>
     )
   }
 }
 
 User.propTypes = {
-  id: PropTypes.string.isRequired
+  username: PropTypes.string.isRequired
 };
 
 User.defaultProps = {
-  id: 'dynm'
+  username: 'dynm'
 };
