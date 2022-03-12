37f09441c0b72ade86a165da384a323d05808307
iamindika <ikolamba2@gmail.com>
2022-03-12 12:48:46 -0500

    move App component logic to News component
    render ranked comments instead of total comments in StoriesGrid component

---
+++ src/components/News.js
@@ -0,0 +1,120 @@
+import React from 'react';
+import Loading from './Loading';
+import PropTypes from 'prop-types';
+import {getStories} from '../utils/api';
+import {getDateString} from '../utils/date';
+
+function StoriesNav({selected, handleClick}) {
+  const storyTypes = ['Top', 'New'];
+
+  return (
+    <ul className="container nav row">
+      {storyTypes.map(type => (
+        <li 
+          key={type} 
+        >
+          <button
+            className="btn link"
+            style={selected === type ? {color: '#FF0800'} : null}
+            onClick={() => handleClick(type)}
+          >{type}</button>
+        </li>
+      ))}
+    </ul>
+  )
+} 
+
+StoriesNav.propTypes = {
+  selected: PropTypes.string.isRequired,
+  handleClick: PropTypes.func.isRequired
+}
+
+function StoriesGrid({stories}) {
+  return (
+    <section className="container grid">
+      {stories.map(story => (
+        <article 
+          className="story"
+          key={story.id}  
+        >
+          <h2>
+            <a  
+              href={story.url}
+              className="story__title"
+            >{story.title}</a> 
+          </h2>
+          <p className="story__desc">
+            by <a href="#">{story.by}</a> on
+            &nbsp;{getDateString(story.time)} with
+            &nbsp;<a href="#">{story.kids ? story.kids.length : 0}</a>
+            &nbsp;ranked comments
+          </p>
+        </article>
+      ))}
+    </section>
+  );
+}
+
+StoriesGrid.propTypes = {
+  stories: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default class News extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      selected: 'Top',
+      error: null
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    const {selected} = this.state;
+
+    getStories(selected)
+      .then(stories => this.setState({[selected]: stories}))
+      .catch(error => this.setState({error: error.message}));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const {selected} = this.state;
+
+    if(prevState.selected !== selected && !this.state[selected]) {
+      this.setState({error: null});
+
+      getStories(selected)
+        .then(stories => this.setState({
+          [selected]: stories,
+        }))
+        .catch(error => this.setState({error: error.message}));
+    }
+  }
+
+  handleClick(type) {
+    this.setState({selected: type})
+  }
+
+  render() {
+    const {selected, error} = this.state;
+
+    return (
+      <React.Fragment>
+        <StoriesNav 
+          selected={this.state.selected}
+          handleClick={this.handleClick}
+        />
+
+        {!error && !this.state[selected]
+          ? <Loading text={`Fetching ${selected} Stories`}/>
+          : null} 
+
+        {error && <p className="error">ERROR: {error}</p>}
+
+        {this.state[selected] && <StoriesGrid stories={this.state[selected]}/>}
+      </React.Fragment>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,124 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Loading from './components/Loading';
-import PropTypes from 'prop-types';
-import {getStories} from './utils/api';
-import {getDateString} from './utils/date';
+import News from './components/News';
 import './index.css';
 
-function StoriesNav({selected, handleClick}) {
-  const storyTypes = ['Top', 'New'];
-
+function App(props) {
   return (
-    <ul className="container nav row">
-      {storyTypes.map(type => (
-        <li 
-          key={type} 
-        >
-          <button
-            className="btn link"
-            style={selected === type ? {color: '#FF0800'} : null}
-            onClick={() => handleClick(type)}
-          >{type}</button>
-        </li>
-      ))}
-    </ul>
+    <React.Fragment>
+      <News/>
+    </React.Fragment>
   )
-} 
-
-StoriesNav.propTypes = {
-  selected: PropTypes.string.isRequired,
-  handleClick: PropTypes.func.isRequired
-}
-
-function StoriesGrid({stories}) {
-  return (
-    <section className="container grid">
-      {stories.map(story => (
-        <article 
-          className="story"
-          key={story.id}  
-        >
-          <h2>
-            <a  
-              href={story.url}
-              className="story__title"
-            >{story.title}</a> 
-          </h2>
-          <p className="story__desc">
-            by <a href="#">{story.by}</a> on
-            &nbsp;{getDateString(story.time)} with
-            &nbsp;<a href="#">{story.descendants}</a>
-            &nbsp;comments
-          </p>
-        </article>
-      ))}
-    </section>
-  );
-}
-
-StoriesGrid.propTypes = {
-  stories: PropTypes.arrayOf(PropTypes.object).isRequired
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selected: 'Top',
-      error: null
-    }
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  componentDidMount() {
-    const {selected} = this.state;
-
-    getStories(selected)
-      .then(stories => this.setState({[selected]: stories}))
-      .catch(error => this.setState({error: error.message}));
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const {selected} = this.state;
-
-    if(prevState.selected !== selected && !this.state[selected]) {
-      this.setState({error: null});
-
-      getStories(selected)
-        .then(stories => this.setState({
-          [selected]: stories,
-        }))
-        .catch(error => this.setState({error: error.message}));
-    }
-  }
-
-  handleClick(type) {
-    this.setState({selected: type})
-  }
-
-  render() {
-    const {selected, error} = this.state;
-
-    return (
-      <React.Fragment>
-        <StoriesNav 
-          selected={this.state.selected}
-          handleClick={this.handleClick}
-        />
-
-        {!error && !this.state[selected]
-          ? <Loading text={`Fetching ${selected} Stories`}/>
-          : null} 
-
-        {error && <p className="error">ERROR: {error}</p>}
-
-        {this.state[selected] && <StoriesGrid stories={this.state[selected]}/>}
-      </React.Fragment>
-    )
-  }
 }
 
 ReactDOM.render(
