35385bcfc2583f835d6600b0a6aa67d466a63f86
iamindika <ikolamba2@gmail.com>
2022-03-17 09:46:31 -0400

    add theming to Comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {ThemeContext} from '../contexts/theme';
 import {getDateString} from '../utils/date';
 import {createMarkup} from '../utils/api';
 
 export default function Comment({comment}) {
   return (
-    <article className="comment">
-      <p className="desc">
-        by <a href="#">{comment.by}</a> on
-        &nbsp;{getDateString(comment.time)}
-      </p>
-      <div 
-        className="comment__text"
-        dangerouslySetInnerHTML={createMarkup(comment.text)}
-      />
-    </article>
+    <ThemeContext.Consumer>
+      {({theme}) => (
+        <article className={`comment comment--${theme}`}>
+          <p className="desc">
+            by <a href="#" 
+                  className={
+                    `${theme === 'dark' ? 'light' : 'dark'}-link`
+                  }>{comment.by}</a> on
+            &nbsp;{getDateString(comment.time)}
+          </p>
+          <div 
+            className="comment__text"
+            dangerouslySetInnerHTML={createMarkup(comment.text)}
+          />
+        </article>
+      )}
+    </ThemeContext.Consumer>
   )
 }
