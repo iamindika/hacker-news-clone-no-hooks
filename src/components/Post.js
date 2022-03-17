7fac3b06464adea1d7d4906ce71e1b31e0afac0e
iamindika <ikolamba2@gmail.com>
2022-03-16 23:30:13 -0400

    utilize theming in Post component

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {ThemeContext} from '../contexts/theme';
 import {getDateString} from '../utils/date';
 
 export default function Post({post, size}) {
   return (
-    <article 
-      className="post" 
-    >
-      <h2 style={{fontSize: size.toString() + 'rem'}}>
-        <a  
-          href={post.url}
-          className="post__title"
-        >{post.title}</a> 
-      </h2>
-      <p className="desc">
-        by <a href="#">{post.by}</a> on
-        &nbsp;{getDateString(post.time)} with
-        &nbsp;<a href="#">{post.kids ? post.kids.length : 0}</a>
-        &nbsp;ranked comments
-      </p>
-    </article>
+    <ThemeContext.Consumer>
+      {({theme}) => (
+        <article 
+          className="post" 
+        >
+          <h2 style={{fontSize: size.toString() + 'rem'}}>
+            <a  
+              href={post.url}
+              className={`post__title post__title--${theme}`}
+            >{post.title}</a> 
+          </h2>
+          <p className="desc">
+            by <a className={theme === 'dark' ? 'light-link' : 'dark-link'} 
+                  href="#">{post.by}</a>
+            &nbsp;on {getDateString(post.time)} with
+            &nbsp;<a  className={theme === 'dark' ? 'light-link' : 'dark-link'} 
+                      href="#">{post.kids ? post.kids.length : 0}</a>
+            &nbsp;ranked comments
+          </p>
+        </article>
+      )}
+    </ThemeContext.Consumer>
   );
 }
 
