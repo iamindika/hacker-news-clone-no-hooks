c2648a36653bcb17ecd5e92393e2125d235e62d4
iamindika <ikolamba2@gmail.com>
2022-03-16 23:32:25 -0400

    utilize ThemeContext provider in App component

--- src/index.js
+++ src/index.js
@@ -3,14 +3,28 @@ import ReactDOM from 'react-dom';
 import News from './components/News';
 import User from './components/User';
 import CommentList from './components/CommentList';
+import {ThemeContext} from './contexts/theme';
 import './index.css';
 
-function App(props) {
-  return (
-    <React.Fragment>
-      <News/>
-    </React.Fragment>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => this.setState(({theme}) => ({
+        theme: theme === 'light' ? 'dark' : 'light'
+      }))
+    }
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider value={this.state}>
+        <News/>
+      </ThemeContext.Provider>
+    )
+  }
 }
 
 ReactDOM.render(
