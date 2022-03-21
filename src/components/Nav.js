2d682aec4d3e23dd79303e41fef1a3228d71f897
iamindika <ikolamba2@gmail.com>
2022-03-20 23:39:26 -0400

    add top level container for theming

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -11,39 +11,41 @@ export default function Nav(props) {
   return (
     <ThemeContext.Consumer>
       {({theme, toggleTheme}) => (
-        <div className="container nav row">
-          <ul className="nav__list row">
-            <li> 
-              <NavLink
-                exact
-                to="/"
-                activeStyle={activeStyle}
-                className={
-                  `link ${theme === 'dark' ? 'light-link' : 'dark-link'}`
-                }
-              >Top</NavLink>
-            </li>
-            <li> 
-              <NavLink
-                to="/new"
-                activeStyle={activeStyle}
-                className={
-                  `link ${theme === 'dark' ? 'light-link' : 'dark-link'}`
-                }
-              >New</NavLink>
-            </li>
-          </ul>
-          <div onClick={toggleTheme}>
-            {theme === 'dark' 
-              ? <MdLightMode 
-                  className='icon'
-                  color={'rgb(253, 255, 0)'}
-                  size={40}
-                />
-              : <MdDarkMode 
-                  className='icon'
-                  size={40}
-                />}
+        <div className={`bg-${theme}`}>
+          <div className="container nav row">
+            <ul className="nav__list row">
+              <li> 
+                <NavLink
+                  exact
+                  to="/"
+                  activeStyle={activeStyle}
+                  className={
+                    `link ${theme === 'dark' ? 'light-link' : 'dark-link'}`
+                  }
+                >Top</NavLink>
+              </li>
+              <li> 
+                <NavLink
+                  to="/new"
+                  activeStyle={activeStyle}
+                  className={
+                    `link ${theme === 'dark' ? 'light-link' : 'dark-link'}`
+                  }
+                >New</NavLink>
+              </li>
+            </ul>
+            <div onClick={toggleTheme}>
+              {theme === 'dark' 
+                ? <MdLightMode 
+                    className='icon'
+                    color={'rgb(253, 255, 0)'}
+                    size={40}
+                  />
+                : <MdDarkMode 
+                    className='icon'
+                    size={40}
+                  />}
+            </div>
           </div>
         </div>
       )}
