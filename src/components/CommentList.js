97be0244de1383b0eef216225dd8a8b172abe967
iamindika <ikolamba2@gmail.com>
2022-03-21 17:32:18 -0400

    utilize query string on location prop to acquire post id and render post
    move comment state and logic to comment component

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,96 +1,60 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Nav from './Nav';
 import Loading from './Loading';
 import Post from './Post';
 import Comment from './Comment';
 import {ThemeContext} from '../contexts/theme';
 import {getPostDetails} from '../utils/api';
+import queryString from 'query-string';
 
+const style = {
+  paddingTop: '2rem',
+  minHeight: '100vh'
+}
 export default class CommentList extends React.Component {
   constructor(props) {
     super(props);
 
       this.state = {
         post: null,
-        comments: null,
         error: null
       }
   }
 
   componentDidMount() {
-    getPostDetails(this.props.id)
+    const {id} = queryString.parse(this.props.location.search);
+
+    getPostDetails(id)
       .then(post => this.setState({post}))
       .catch(error => this.setState({error: error.message}));
   }
 
-  componentDidUpdate() {
-    const {post, comments} = this.state;
-
-    if(post && post.kids && !comments) {
-      Promise.all(post.kids.map(commentId => getPostDetails(commentId)))
-        .then(comments => this.setState({comments}))
-        .catch(error => this.setState({error}))
-    }
-  }
-
   render() {
-    const {post, comments, error} = this.state;
-    
+    const {post, error} = this.state;
+
     return (
       <ThemeContext.Consumer>
         {({theme}) => (
           <div className={`bg-${theme}`}>
-            <Nav/>
-            
-            {!post && !error 
-              ? <Loading text="Fetching Post"/>
-              : null}
+            {!post && <Loading text="Fetching Post"/>}
+
+            {error && <p className='error'>ERROR: {error}</p>}
 
             {post && (
               <div 
                 className="container"
-                style={{marginTop: '2rem'}}
+                style={style}
               >
                 <Post 
                   post={post}
                   size={3}  
                 />
+
+                {/* {post.descendants && <Comment comments={post.kids.slice(0, 50)}/>} */}
               </div> 
             )}
-
-            {post && !comments
-              ? <Loading text="Fetching Comments"/>
-              : null}
-
-            {comments && (
-              <section
-                className="container grid"
-              >
-                {comments.filter(comment => !comment.deleted)
-                  .slice(0, 50)
-                  .map(comment => (
-                    <Comment 
-                      key={comment.id}
-                      comment={comment}
-                    />
-                  ))
-                }
-              </section>
-            )}
-
-            {error && <p className='error'>ERROR: {error}</p>}
           </div>
         )}
       </ThemeContext.Consumer>
     )
   }
-}
-
-CommentList.propTypes = {
-  id: PropTypes.number.isRequired
-}
-
-CommentList.defaultProps = {
-  id: 30661852
 }
