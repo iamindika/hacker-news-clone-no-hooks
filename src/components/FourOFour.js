55e8a9228b579926c74a2ce3985c7c6c44ba6c1d
iamindika <ikolamba2@gmail.com>
2022-03-24 18:44:07 -0400

    utilize theme context and add background class theme

--- src/components/FourOFour.js
+++ src/components/FourOFour.js
@@ -1,6 +1,13 @@
 import React from "react";
+import {ThemeContext} from "../contexts/theme";
 
 export default function FourOFour(props) {
   console.log(props);
-  return <p className="container error">404</p>
+  return (
+    <ThemeContext.Consumer>
+      {({theme}) => (
+        <p className={`error bg-${theme}`}>404</p>
+      )}
+    </ThemeContext.Consumer>
+  )
 }
