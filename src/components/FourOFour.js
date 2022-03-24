6b3ae068adc95f30d7f0a3c74c77ceb91a9b09a4
iamindika <ikolamba2@gmail.com>
2022-03-24 19:10:50 -0400

    remove console log

--- src/components/FourOFour.js
+++ src/components/FourOFour.js
@@ -2,7 +2,6 @@ import React from "react";
 import {ThemeContext} from "../contexts/theme";
 
 export default function FourOFour(props) {
-  console.log(props);
   return (
     <ThemeContext.Consumer>
       {({theme}) => (
