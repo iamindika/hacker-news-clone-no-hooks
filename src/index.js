1e7d9feeefb1d666f5d07ae93bc7a62803975c81
iamindika <ikolamba2@gmail.com>
2022-03-24 18:23:00 -0400

    utilize class fields in main index.js

--- src/index.js
+++ src/index.js
@@ -12,15 +12,11 @@ import {
 import './index.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      theme: 'light',
-      toggleTheme: () => this.setState(({theme}) => ({
-        theme: theme === 'light' ? 'dark' : 'light'
-      }))
-    }
+  state = {
+    theme: 'light',
+    toggleTheme: () => this.setState(({theme}) => ({
+      theme: theme === 'light' ? 'dark' : 'light'
+    }))
   }
 
   render() {
