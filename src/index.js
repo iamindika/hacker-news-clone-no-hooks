85145ec4f2299dabd74e464b0a17c86429e1a0c8
iamindika <ikolamba2@gmail.com>
2022-03-24 18:45:51 -0400

    utilize Switch component with routes and add a catch all 404 Route component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import Nav from './components/Nav';
 import News from './components/News';
 import User from './components/User';
 import CommentWrapper from './components/CommentWrapper';
+import FourOFour from './components/FourOFour';
 import {ThemeContext} from './contexts/theme';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom';
 import './index.css';
 
@@ -25,10 +27,13 @@ class App extends React.Component {
         <ThemeContext.Provider value={this.state}>
           <Nav/>
 
-          <Route exact path='/' component={News}/>
-          <Route path='/new' component={News}/>
-          <Route path='/user' component={User}/>
-          <Route path='/post' component={CommentWrapper}/>
+          <Switch>
+            <Route exact path='/' component={News}/>
+            <Route path='/new' component={News}/>
+            <Route path='/user' component={User}/>
+            <Route path='/post' component={CommentWrapper}/>
+            <Route component={FourOFour}/>
+          </Switch>
         </ThemeContext.Provider>
       </Router>
     )
