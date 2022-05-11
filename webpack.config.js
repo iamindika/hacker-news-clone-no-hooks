09aab07799eeb89f3e28164bcfc56bea7d524152
iamindika <ikolamba2@gmail.com>
2022-05-11 15:09:45 -0400

    add copy plugin to redirect all requests on hosting site to index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -17,6 +18,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "_redirects" }
+      ]
     })
   ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
